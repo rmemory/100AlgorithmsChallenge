6d79c2064c17e6afcba836ea31002a115b2430f1
Richard Memory <richardfmemory@gmail.com>
2018-09-10 19:42:21 -0600

    Opps missed one

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,17 @@
-function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
+function arrayMaxConsecutiveSum(inputArray, k) {
+	const sumArray = [];
 
+	for (let i = 0; i < inputArray.length - (k - 1); i++) {
+		let sum = 0;
+
+		for (let j = 0; j < k; j++) {
+			sum += inputArray[i + j];
+		}
+
+		sumArray.push(sum);
+	}
+
+	return Math.max(...sumArray);
 }
 
 console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
